216ec9c7c7b25bb64d07b3c797e10caa4f65018c
jschneider3932 <jschneider3932@conestogac.on.ca>
2023-03-28 12:29:19 -0400

    Changed default number of posts shown, added a button to load more posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,56 +1,65 @@
 console.log("hello world")
 
-const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
 const spinnerBox = document.getElementById('spinner-box')
-
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
-    success: function(response){
-        console.log('success',response.text)
-        helloWorldBox.textContent = response.text
-    },
-    error: function(error){
-        console.log('error', error)
-    }
-})
+const loadBtn = document.getElementById('load-btn')
+const endBox = document.getElementById('end-box')
 
 
+let visible = 3
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response){
-        console.log(response)
-        const data = response.data
-        setTimeout(() => {
-            console.log(data)
-            data.forEach(el => {
-                postsBox.innerHTML +=  `
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">${el.title}</h5>
-                            <p class="card-text">${el.body}</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Details</a>
-                                </div>
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Like</a>
+const getData = () => {
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`,
+        success: function(response){
+            console.log(response)
+            const data = response.data
+            setTimeout(() => {
+                console.log(data)
+                data.forEach(el => {
+                    postsBox.innerHTML +=  `
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.title}</h5>
+                                <p class="card-text">${el.body}</p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-1">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col-1">
+                                        <a href="#" class="btn btn-primary">Like</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                `
-                spinnerBox.classList.add('not-visible')
-            });
-        }, 100);
+                    `
+                    spinnerBox.classList.add('not-visible')
+                });
+            }, 100)
+            console.log(response.size)
+            if(response.size === 0){
+                endBox.textContent = 'No Posts added Yet...'
+            }
+            else if(response.size <= visible){
+                loadBtn.classList.add('not-visible')
+                endBox.textContent = 'No more posts to load..'
+            }
+    
+        },
+        error: function(response){
+            console.log(error)  
+        }
+    })
+}
 
-    },
-    error: function(response){
-        console.log(error)  
-    }
+
+loadBtn.addEventListener('click', () =>{
+    spinnerBox.classList.remove('not-visible')
+    visible +=3
+    getData()
 })
+
+getData()
